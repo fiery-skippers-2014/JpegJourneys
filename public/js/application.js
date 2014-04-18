65be4e86a6504ede938d8876d2a0fbb46e4ff9a1
Administrator <administrator@Administrators-MacBook-Pro.local>
2014-04-18 16:51:58 -0700

    jdfbf

--- public/js/application.js
+++ public/js/application.js
@@ -12,80 +12,8 @@ $('document').ready(function() {
 // //// HANDLER
 var handler = function(event){
   event.preventDefault();
-  // var mappy = new GoogleMap();
-  // console.log("HANDLER");
-  // mappy.initialize_me();
-  // mappy.code_it();
- doFlickr();
+  doFlickr();
 }
-
-
-// ///GOOGLE MAPS
-// GoogleMap = function() {
-//   event.preventDefault();
-// }
-
-// GoogleMap.prototype = {
-//   initialize_me: function(d) {
-//     console.log("Initialize Me")
-
-//     var jqxhr = $.ajax({
-//       url: '/photos',
-//       type: "POST",
-
-//       success: function(){
-//         console.log("Successful initialize");
-//         geocoder = new google.maps.Geocoder();
-//         var latlng = new google.maps.LatLng(37.774, -122.419);
-//         var mapOptions = {
-//           zoom: 8,
-//           center: latlng
-//         }
-//         map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
-//       },
-//       fail: function(){
-//         console.log("Failed initialize");
-//       }
-//     })
-//   },
-
-
-  // code_it: function(){
-
-  //   var jqxhr = $.ajax({
-  //     url: '/photos',
-  //     type: "POST",
-  //     data: {tags: $("#address").val()},
-
-  //     success: function(data){
-  //       console.log("Success!!" + data);
-  //       console.log("Making the map!")
-
-  //       var address = document.getElementById('address').value;
-  //       geocoder.geocode( { 'address': address}, function(results, status) {
-  //         if (status == google.maps.GeocoderStatus.OK) {
-  //           map.setCenter(results[0].geometry.location);
-  //           var marker = new google.maps.Marker({
-  //             map: map,
-  //             position: results[0].geometry.location
-  //           });
-  //         } else {
-  //           alert('Geocode was not successful for the following reason: ' + status);
-  //         }
-  //       });
-  //     },
-  //     fail: function(data){
-  //       console.log("Fail" + data);
-  //     }
-  //   })
-  // }
-// }
-
-
-
-
-
-
 
   ///FLICKR
   var doFlickr = function(){
