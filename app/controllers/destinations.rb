895568a9d7498c2378bd9f73c4bf20a917b1510f
Alexandra & Hembaba <pair+Answart.hembaba@devbootcamp.com>
2014-04-18 13:56:33 -0700

    Create function routes except for destination
    * Can create journey
    
    TODO: Create a destination in the database.

--- app/controllers/destinations.rb
+++ app/controllers/destinations.rb
@@ -1,21 +1,35 @@
 get "/users/:user_id/journeys/:journey_id/destinations/new" do
   @user = current_user
+  @journey = Journey.find(params[:journey_id])
   erb :'/destination/destination_create'
 end
 
 
 post "/users/:user_id/journeys/:journey_id/destinations" do
-  @destination = Destination.new(
+  p "-"*20
+  # params
+  @this_journey = Journey.find(params[:journey_id].to_i)
+  p @this_journey
+  p "-"*20
+  # @taggs = params[:tags]
+  # @journey = Journey.find(:journey_id])
+  # p "$$$$$$$$$$$$$$$"
+  # p @journey
+  # p "$$$$$$$$$$$$$$$$"
+  @destination = Destination.create(
     place: params[:tags],
-    journey_id: params[:journey_id],
+    journey_id: @this_journey.id,
   )
-  if @journey.save
+  p "$$$$$$$$$$$$$$$$$$$"
+  p @destination
+  if @destination
     flash[:success] = "Destination created!"
     @journey = Journey.find(@destination.journey_id)
     @tags = params[:tags]
     p @tags
     picture = flickr.photos.search(tags:@tags, per_page: "1").first
     @url = FlickRaw.url(picture)
+    p @url
     # redirect "/users/#{@journey.user_id}/journeys/#{@journey.id}"
   else
     flash[:error] = "Destination was not created!"
@@ -27,16 +41,16 @@ end
 
 
 
-get '/photos' do
-  erb :photos
-end
+# get '/photos' do
+#   erb :photos
+# end
 
 
 
-get '/photos/new' do
-  erb :get_photos
-end
+# get '/photos/new' do
+#   erb :get_photos
+# end
 
-post '/photos' do
+# post '/photos' do
 
-end
+# end
--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -2,3 +2,18 @@ get '/' do
   @journeys = Journey.all
   erb :index
 end
+
+
+
+
+
+get '/photos/new' do
+  erb :get_photos
+end
+
+post '/photos' do
+  @tags = params[:tags]
+  p @tags
+  picture = flickr.photos.search(tags:@tags, per_page: "1").first
+  @url = FlickRaw.url(picture)
+end
